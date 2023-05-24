e08288f345427fb8c05b248c9e7fdee202bf218a
Christian Seiler <christian@christianseiler.ch>
2023-05-24 13:54:20 +0200

    Fix Action

--- web/index.php
+++ web/index.php
@@ -4,9 +4,9 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-$dir = __DIR__;
-require "$dir/../vendor/autoload.php";
-require "$dir/../vendor/yiisoft/yii2/Yii.php";
-$config = require "$dir/../config/web.php";
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+$config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
